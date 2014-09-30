0512b24bc430640d6e4354894b638dd1f6b873de
Raif Harik <reharik@gmail.com>
2014-09-30 17:28:44 -0500

    adding archive user

---
+++ src/Projects/Domain/MF.Core.MessageBinders/MessageBinders/ArchiveTrainerMessageBinder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using EventStore.ClientAPI;
+using MF.Core.Infrastructure;
+using MF.Core.Infrastructure.Mongo;
+using MF.Core.Messages.Command;
+using MF.Core.ReadModel.Model;
+using Newtonsoft.Json;
+
+namespace MF.Core.MessageBinders.MessageBinders
+{
+    public class ArchiveTrainerMessageBinder : MessageBinderBase
+    {
+        private readonly IMongoRepository _mongoRepository;
+
+        public ArchiveTrainerMessageBinder(IMongoRepository mongoRepository, IEventStoreConnection eventStoreConnection)
+            : base(eventStoreConnection)
+        {
+            _mongoRepository = mongoRepository;
+        }
+
+        public void AcceptRequest(Guid trainerId, bool archive)
+        {
+
+
+            var loginUser = new LoginUser(user.Id, password, user.UserName);
+            // noise
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Command Created: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(JsonConvert.SerializeObject(loginUser));
+            // noise
+
+            PostEvent(loginUser,Guid.NewGuid());
+        }
+    }
+}
---
+++ src/Projects/Domain/MF.Core.Messages/Command/ArchiveUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EventStore.ClientAPI;
+using MF.Core.Infrastructure.SharedModels;
+
+namespace MF.Core.Messages.Command
+{
+    public class ArchiveUser : IGESEvent
+    {
+        public ArchiveUser(Guid trainerId)
+        {
+            TrainerId = trainerId;
+            EventType = "ArchiveUser";
+        }
+
+        public Guid TrainerId { get; private set; }
+        public string EventType { get; private set; }
+        public Position? OriginalPosition { get; set; }
+    }
+
+    public class UnArchiveUser : IGESEvent
+    {
+        public UnArchiveUser(Guid trainerId)
+        {
+            TrainerId = trainerId;
+            EventType = "UnArchiveUser";
+        }
+
+        public Guid TrainerId { get; private set; }
+        public string EventType { get; private set; }
+        public Position? OriginalPosition { get; set; }
+    }
+}
